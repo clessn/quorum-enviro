2ec31fbb4a733212cf4ae5ce06c8971143fd3086
JerGil <jeremy.gilbert.1@ulaval.ca>
2022-10-03 12:54:11 -0400

    Cleaning Q76

--- cleaning.R
+++ cleaning.R
@@ -9,7 +9,7 @@ source("functions.R", encoding = "UTF-8")
 
 
 # Data ####
-Data <- haven::read_sav("_SharedFolder_quorum-enviro/data/ULA011-données.Sav") %>% 
+Data <- haven::read_sav("_SharedFolder_transition/data/ULA011-données.Sav") %>% 
   filter(PROV == 11) %>% 
   mutate(id = 1:nrow(.))
 
@@ -17,6 +17,10 @@ Data <- haven::read_sav("_SharedFolder_quorum-enviro/data/ULA011-données.Sav") 
 ForNadj <- data.frame(id = Data$id,
                       postal_code = Data$CP,
                       age = 2022-Data$Q7)
+
+CleanData <- data.frame(id = Data$id,
+                        postal_code = Data$CP,
+                        age = 2022-Data$Q7)
 
 # Gender ####
 table(Data$SEXE)
@@ -79,3 +83,41 @@ ForNadj$proprio[Data$Q14 != 2] <- 0
 table(ForNadj$proprio)
 
 saveRDS(ForNadj, "_SharedFolder_quorum-enviro/data/cleanData/data.rds")
+
+
+# Question 76 ####
+
+#  Il y a beaucoup d’informations qui circulent à propos de l’origine du coronavirus, le 
+#virus qui provoque la COVID-19. À votre avis, le coronavirus est:
+
+table(Data$Q76A)
+CleanData$covidOriginByChinaA[Data$Q76A == 1 | Data$Q76A == 3] <- 0 # N'est pas établie ou ne sait pas
+CleanData$covidOriginByChinaA[Data$Q76A == 2] <- 1 # Chine
+table(CleanData$covidOriginByChinaA)
+
+table(Data$Q76A)
+CleanData$covidOriginNotKnownA[Data$Q76A == 2 | Data$Q76A == 3] <- 0 #Chine ou ne sait pas
+CleanData$covidOriginNotKnownA[Data$Q76A == 1] <- 1 # n'est pas établie
+table(CleanData$covidOriginNotKnownA)
+
+
+table(Data$Q76B)
+CleanData$covidOriginByChinaB[Data$Q76B == 1] <- 0 # N'est pas établie
+CleanData$covidOriginByChinaB[Data$Q76B == 2] <- 1 # Chine
+table(CleanData$covidOriginByChinaB)
+
+table(Data$Q76C)
+CleanData$covidOriginByChinaC[Data$Q76C == 1] <- 1 # Très certainement vrai
+CleanData$covidOriginByChinaC[Data$Q76C == 2] <- 0.67 # Probablement vrai
+CleanData$covidOriginByChinaC[Data$Q76C == 3] <- 0.33 # Probablement faux
+CleanData$covidOriginByChinaC[Data$Q76C == 4] <- 0 # Très certainement faux
+table(CleanData$covidOriginByChinaC)
+
+table(Data$Q76D)
+CleanData$covidOriginByChinaD[Data$Q76D == 1] <- 1 # Très certainement vrai
+CleanData$covidOriginByChinaD[Data$Q76D == 2] <- 0.75 # Probablement vrai
+CleanData$covidOriginByChinaD[Data$Q76D == 5] <- 0.50 # Ne sait pas
+CleanData$covidOriginByChinaD[Data$Q76D == 3] <- 0.25 # Probablement faux
+CleanData$covidOriginByChinaD[Data$Q76D == 4] <- 0 # Très certainement faux
+table(CleanData$covidOriginByChinaD)
+
